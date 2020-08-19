e5ad4d433795ec6fb7837922e83463e1c25359ec
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-08-19 02:02:46 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
-end
+    if array.size == 1 
+        return array.join 
+    end 
+    if array.size == 2
+        first=array[0]
+        second=array[1]
+        return "#{first} and #{second}"
+    end 
+    last=array.size - 1
+     new_array= array.slice(0,last)
+     string=new_array.join(", ")
+     string << ", and #{array[-1]}"
+     string
+  
+  end
