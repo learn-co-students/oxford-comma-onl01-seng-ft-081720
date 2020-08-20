c58125ba3bd48962b4d7176681cdfa7bbac267b8
NickRylander <nick.rylander@outlook.com>
2020-08-19 18:25:27 -0700

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+# returns a string without any additional formatting when given a 1-element array (FAILED - 1)
+# adds 'and' between elements when given a 2-element array (FAILED - 2)
+# adds commas plus a final 'and' when given a 3-element array (FAILED - 3)
+# correctly formats arrays of lengths greater than three (FAILED - 4)
 
+
+def oxford_comma(array)
+    if array.length == 2
+        array.join(" and ")
+    elsif array.length > 2
+        array_2 = array.pop
+        array_comma = array.join(", ")
+        array_comma << ", and #{array_2}"
+    else
+        array.join
+    end
 end
