12592de21184c042c0d02c67807b9e2c6eacf69f
“saschakala” <mxsaschakala@gmail.com>
2020-08-21 14:31:43 -0600

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+array.join
+if  array.count == 1
+    array.join
+elsif array.count == 2
+    array.join(" and ")
+else array.count > 2
+    array[-1].prepend "and " 
+    array.join (", ") 
+end
 end
