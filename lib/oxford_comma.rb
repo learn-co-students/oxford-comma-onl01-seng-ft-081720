7d4b01ee9d71f0183dbbad0e757ae8cfbc4cb484
redebron20 <redebron20@gmail.com>
2020-08-21 21:05:54 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+    if array.length == 1
+        array.join("")
+    elsif array.length == 2
+        array.join(' and ')
+    elsif array.length > 2
+        array_copy = array
+        array_copy[-1] = "and #{array_copy[-1]}"
+        array_copy.join(', ')
+    end
 
 end
+
+       
